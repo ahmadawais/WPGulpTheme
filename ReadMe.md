a91bd017e57a244c86fd8e5ba46415f10160e6d6
Ahmad Awais <mrahmadawais@gmail.com>
2015-12-02 17:54:27 +0500

    Final changes in comments at Gulpfile.js

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,14 @@
 #WPGulpTheme
 A theme to demonstrate simplest implementation of WPGulp Boilerplate.
 
+##How?
+- Configure the project paths in gulpfile.js (Line #23 t0 #38)
+- Open the project folder in the terminal and type `sudo npm install`  wait for the files to get downloaded. It will take about 2 minutes to add a node_modules folder inside your project.
+- Once the download is complete type `gulp` and boom!
+
+##Can You Build Just What's Needed?
+Sure! Here you go, it's called [WPGulp](https://github.com/ahmadawais/WPGulp)
+
 ##To do 
 - [x] Add base files
 - [ ] Add more info
--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,44 @@
  * @author Ahmad Awais (@mrahmadawais)
  */
 
+ /**
+  * Configuration.
+  *
+  * Project Configuration for gulp tasks.
+  *
+  * Edit the variables as per your project requirements.
+  */
+
+	var project             = 'WPGulpTheme'; // Name
+
+	var styleSRC            = './assets/css/style.scss'; // Path to main .scss file
+	var styleDestination    = './'; // Path to place the compiled CSS file
+									// Defualt set to root folder
+
+
+	var jsVendorSRC         = './assets/js/vendors/*.js'; // Path to JS vendors folder
+	var jsVendorDestination = './assets/js/'; // Path to place the compiled JS vendors file
+	var jsVendorFile        = 'vendors'; // Compiled JS vendors file name
+										// Default set to vendors i.e. vendors.js
+
+
+	var jsCustomSRC         = './assets/js/custom/*.js'; // Path to JS custom scripts folder
+	var jsCustomDestination = './assets/js/'; // Path to place the compiled JS custom scripts file
+	var jsCustomFile        = 'custom'; // Compiled JS custom file name
+										// Default set to custom i.e. custom.js
+
+
 /**
  * Load Plugins.
  *
  * Load gulp plugins and assing them semantic names.
  */
-	var gulp     = require('gulp'); // Gulp of-course
+	var gulp         = require('gulp'); // Gulp of-course
 
 	// CSS related plugins.
 	var sass         = require('gulp-sass'); // Gulp pluign for Sass compilation
 	var autoprefixer = require('gulp-autoprefixer'); // Autoprefixing magic
 	var minifycss    = require('gulp-uglifycss'); // Minifies CSS files
-	var cmq          = require('gulp-combine-media-queries'); // Combines media queries
 
 	// JS related plugins.
 	var concat       = require('gulp-concat'); // Concatenates JS files
@@ -35,113 +61,111 @@
 	var sourcemaps   = require('gulp-sourcemaps'); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css)
 	var notify       = require('gulp-notify'); // Sends message notification to you
 
-/**
- * Configuration.
- *
- * Project Configuration for gulp tasks.
- */
-
-	var project             = 'WPGulpTheme';
-	var source              = './assets/';
-
-	var styleSRC            = './assets/css/style.scss';
-	var styleDestination    = './';
-
-	var jsVendorSRC         = './assets/js/vendors/*.js';
-	var jsVendorDestination = './assets/js/';
-	var jsVendorFile        = 'vendors';
-
-	var jsCustomSRC         = './assets/js/custom/*.js';
-	var jsCustomDestination = './assets/js/';
-	var jsCustomFile        = 'custom';
-
 
 /**
  * Task: styles
  *
+ * Compiles Sass, Autoprfixes it and Minifies CSS.
+ *
  * This task does the following:
  * 		1. Gets the source scss file
- * 		2. Compiles it to CSS
- * 		3. Autoprefixes it
- * 		4. Combines the MediaQueries
- * 		5. Minifies the CSS file
- * 		6. Renames the minified CSS file with suffix .min.css
- *
- * Sass output styles: https://web-design-weekly.com/2014/06/15/different-sass-output-styles/
-*/
+ * 		2. Compiles Sass to CSS
+ * 		3. Writes Sourcemaps for it
+ * 		4. Autoprefixes it and generates style.css
+ * 		5. Renames the CSS file with suffix .min.css
+ * 		6. Minifies the CSS file and generates style.min.css
+ */
 gulp.task('styles', function () {
-	return 	gulp.src( styleSRC )
-				.pipe( sourcemaps.init() )
-				.pipe( sass( {
-					errLogToConsole: true,
-					outputStyle: 'compact',
-					//outputStyle: 'compressed',
-					// outputStyle: 'nested',
-					// outputStyle: 'expanded',
-					precision: 10
-				} ) )
-				.pipe( sourcemaps.write( { includeContent: false } ) )
-				.pipe( sourcemaps.init( { loadMaps: true } ) )
-				.pipe( autoprefixer(
-					'last 2 version',
-					'> 1%',
-					'safari 5',
-					'ie 8',
-					'ie 9',
-					'opera 12.1',
-					'ios 6',
-					'android 4' ) )
-				.pipe( sourcemaps.write ( '.' ) )
-				.pipe( gulp.dest( styleDestination ) )
-				.pipe( filter( '**/*.css' ) ) // Filtering stream to only css files
+ 	gulp.src( styleSRC )
+		.pipe( sourcemaps.init() )
+		.pipe( sass( {
+			errLogToConsole: true,
+			outputStyle: 'compact',
+			//outputStyle: 'compressed',
+			// outputStyle: 'nested',
+			// outputStyle: 'expanded',
+			precision: 10
+		} ) )
+		.pipe( sourcemaps.write( { includeContent: false } ) )
+		.pipe( sourcemaps.init( { loadMaps: true } ) )
+		.pipe( autoprefixer(
+			'last 2 version',
+			'> 1%',
+			'safari 5',
+			'ie 8',
+			'ie 9',
+			'opera 12.1',
+			'ios 6',
+			'android 4' ) )
 
-				.pipe( rename( { suffix: '.min' } ) )
-				.pipe( minifycss( {
-					maxLineLen: 80
-				}))
-				.pipe( gulp.dest( styleDestination ) )
-				.pipe( notify( { message: 'TASK: "styles" Completed!', onLast: true } ) )
+		.pipe( sourcemaps.write ( styleDestination ) )
+		.pipe( gulp.dest( styleDestination ) )
+
+
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( minifycss( {
+			maxLineLen: 10
+		}))
+		.pipe( gulp.dest( styleDestination ) )
+		.pipe( notify( { message: 'TASK: "styles" Completed!', onLast: true } ) )
 });
 
 
 /**
  * Task: vendorJS
  *
- * Look at src/js and concatenate those files, send them to assets/js where we then minimize the concatenated file.
-*/
+ * Concatenate and uglify vendor JS scripts.
+ *
+ * This task does the following:
+ * 		1. Gets the source folder for JS vendor files
+ * 		2. Concatenates all the files and generates vendors.js
+ * 		3. Renames the JS file with suffix .min.js
+ * 		4. Uglifes/Minifies the JS file and generates vendors.min.js
+ */
 gulp.task( 'vendorsJs', function() {
-	return 	gulp.src( jsVendorSRC )
-				.pipe( concat( jsVendorFile + '.js' ) )
-				.pipe( gulp.dest( jsVendorDestination ) )
-				.pipe( rename( {
-					basename: jsVendorFile,
-					suffix: '.min'
-				}))
-				.pipe( uglify() )
-				.pipe( gulp.dest( jsVendorDestination ) )
-				.pipe( notify( { message: 'TASK: "vendorsJs" Completed!', onLast: true } ) );
+	gulp.src( jsVendorSRC )
+		.pipe( concat( jsVendorFile + '.js' ) )
+		.pipe( gulp.dest( jsVendorDestination ) )
+		.pipe( rename( {
+			basename: jsVendorFile,
+			suffix: '.min'
+		}))
+		.pipe( uglify() )
+		.pipe( gulp.dest( jsVendorDestination ) )
+		.pipe( notify( { message: 'TASK: "vendorsJs" Completed!', onLast: true } ) );
 });
 
 
 /**
  * Task: customJS
  *
- * Look at src/js and concatenate those files, send them to assets/js where we then minimize the concatenated file.
-*/
+ * Concatenate and uglify custom JS scripts.
+ *
+ * This task does the following:
+ * 		1. Gets the source folder for JS custom files
+ * 		2. Concatenates all the files and generates custom.js
+ * 		3. Renames the JS file with suffix .min.js
+ * 		4. Uglifes/Minifies the JS file and generates custom.min.js
+ */
 gulp.task( 'customJS', function() {
-	return 	gulp.src( jsCustomSRC )
-				.pipe( concat( jsCustomFile + '.js' ) )
-				.pipe( gulp.dest( jsCustomDestination ) )
-				.pipe( rename( {
-					basename: jsCustomFile,
-					suffix: '.min'
-				}))
-				.pipe( uglify() )
-				.pipe( gulp.dest( jsCustomDestination ) )
-				.pipe( notify( { message: 'TASK: "customJs" Completed!', onLast: true } ) );
+ 	gulp.src( jsCustomSRC )
+		.pipe( concat( jsCustomFile + '.js' ) )
+		.pipe( gulp.dest( jsCustomDestination ) )
+		.pipe( rename( {
+			basename: jsCustomFile,
+			suffix: '.min'
+		}))
+		.pipe( uglify() )
+		.pipe( gulp.dest( jsCustomDestination ) )
+		.pipe( notify( { message: 'TASK: "customJs" Completed!', onLast: true } ) );
 });
 
- // Watch Task
+ /**
+  * Watch Tasks.
+  *
+  * Watches for file changes and runs specific tasks.
+  */
+
  gulp.task( 'default', [ 'styles', 'vendorsJs', 'customJS' ], function () {
  	gulp.watch( './assets/css/**/*.scss', [ 'styles' ] );
  	gulp.watch( './assets/js/vendors/*.js', [ 'vendorsJs' ] );
